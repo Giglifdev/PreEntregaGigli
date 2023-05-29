61b2023b54e9a746e964f19d4372ee86539e0a34
faustog <fausg92@gmail.com>
2023-05-29 12:06:34 -0300

    coment

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 //welcome//
-let welcome = prompt("Bienvenido a SuperTech, por favor ingresa tu nombre");
+let welcome = prompt("Bienvenido a HardwareHaven, por favor ingresa tu nombre");
     console.log("Hola, " + welcome + "!");
+
+    let southAmerica = ["Brazil" , "Argentina" , "Peru" , "Uruguay"]
+let northAmerica = ["USA" , "Canada"]
+let europe = ["Spain" , "France" , "Germany" , "United Kingdom" , "Italy"]
+
+let chosenContinent = prompt(`
+Choose a Continent: 
+1: South America
+2: North America
+3: Europe
+`);
+
+let filteredCountries;
+
+if (chosenContinent === "South America" , 1) {
+    filteredCountries = southAmerica.filter(country => true);
+} else if (chosenContinent === "North America", 2) {
+    filteredCountries = northAmerica.filter(country => true);
+} else if (chosenContinent === "Europe", 3) {
+    filteredCountries = europe.filter(country => true);
+} else {
+    alert("Invalid Option");
+}
+
+console.log(filteredCountries);
 
 //Store//
 let StoreBrands = alert("seleccione el tipo de accesorio y acontinuacion se le mostrara una marca a elegir, siga los pasos.");
 
+const selectedproducts = [];
+const cartPrices = [];
 
 
-let Store = prompt(`
-1: Audio
-2: Keyboards
-3: Mouse
-4: Mousepads
-5: Notebooks
-6: Desktop
-`)
+const Store = prompt(`
+    1: Audio
+    2: Keyboards
+    3: Mouse
+    4: Mousepads
+    5: Notebooks
+    6: Desktop
+`);
+
+
+
+
 
 //condicionales//
 
@@ -31,19 +62,29 @@ switch (Store) {
     `);
     switch (brand1) {
         case "1":
-            console.log("you selected Razer audio products");
+            selectedproducts.push("Razer audio Products");
+            cartPrices.push(120000);
+            console.log("you selected Razer audio Products");
         break;
         case "2":
-            console.log("you selected HyperX audio products");
+            selectedproducts.push("HyperX audio Products");
+            cartPrices.push(66309);
+            console.log("you selected HyperX audio Products");
         break;
         case "3":
-            console.log("you selected Logitech audio products");
+            selectedproducts.push("Logitech audio Products")
+            cartPrices.push(30000);
+            console.log("you selected Logitech audio Products");
         break;
         case "4":
-            console.log("you selected Redragon audio products");
+            selectedproducts.push("Redragon audio Products");
+            cartPrices.push(6600);
+            console.log("you selected Redragon audio Products");
         break;
         case "5":
-            console.log("you selected Samsung Galaxy audio products");
+            selectedproducts.push("Samsung Galaxy audio Products");
+            cartPrices.push(76000);
+            console.log("you selected Samsung Galaxy audio Products");
         break;
         default:
             console.log("invalid option");    
@@ -60,19 +101,29 @@ switch (Store) {
     `)
     switch (brand2) {
         case "1":
-            console.log("you selected Razer keyboard  products");
+            selectedproducts.push("Razer keyboard Products");
+            cartPrices.push(80000);
+            console.log("you selected Razer keyboard  Products");
         break;
         case "2":
-            console.log("you selected Logitech keyboard products");
+            selectedproducts.push("Logitech keyboard Products");
+            cartPrices.push(45000);
+            console.log("you selected Logitech keyboard Products");
         break;
         case "3":
-            console.log("you selected HyperX keyboard products");
+            selectedproducts.push("HyperX keyboard Products");
+            cartPrices.push(27000);
+            console.log("you selected HyperX keyboard Products");
         break;
         case "4":
-            console.log("you selected Redragon keyboard products");
+            selectedproducts.push("Redragon keyboard Products");
+            cartPrices.push(16000);
+            console.log("you selected Redragon keyboard Products");
         break;
         case "5":
-            console.log("you selected Genesis Thor keyboard products");
+            selectedproducts.push("Genesis Thor keyboard Products")
+            cartPrices.push(20000);
+            console.log("you selected Genesis Thor keyboard Products");
         break;
         default:
             console.log("invalid option");    
@@ -83,17 +134,23 @@ switch (Store) {
     Select a brand:
     1: Razer
     2: Logitech
-    3: HyperX
+    3: Logitech
     `)
     switch (brand3) {
         case "1":
-            console.log("you selected Razer mouse products");
+            selectedproducts.push("Razer mouse Products");
+            cartPrices.push(85000);
+            console.log("you selected Razer mouse Products");
         break;
         case "2":
-            console.log("you selected Logitech mouse products");
+            selectedproducts.push("Logitech mouse Products");
+            cartPrices.push(12000);
+            console.log("you selected Logitech mouse Products");
         break;
         case "3":
-            console.log("you selected HyperX mouse products");
+            selectedproducts.push("Logitech mouse Products");
+            cartPrices.push(44000);
+            console.log("you selected Logitech mouse Products");
         break;
         default:
             console.log("invalid option");
@@ -110,19 +167,29 @@ switch (Store) {
     `)
     switch(brand4) {
         case "1":
-            console.log("you selected HyperX mousepad products");
+            selectedproducts.push("HyperX mousepad Products");
+            cartPrices.push(15000);
+            console.log("you selected HyperX mousepad Products");
         break;
         case "2":
-            console.log("you selected Logitech mousepad products");
+            selectedproducts.push("Logitech mousepad Products");
+            cartPrices.push(18000);
+            console.log("you selected Logitech mousepad Products");
         break;
         case "3":
-            console.log("you selected Cooler Master mousepad products");
+            selectedproducts.push("Cooler Master mousepad Products");
+            cartPrices.push(8000);
+            console.log("you selected Cooler Master mousepad Products");
         break;
         case "4":
-            console.log("you selected Genius mousepad products");
+            selectedproducts.push("Genius mousepad Products");
+            cartPrices.push(3000);
+            console.log("you selected Genius mousepad Products");
         break;
         case "5":
-            console.log("you selected Ugo Orizaba mousepad products");
+            selectedproducts.push("Ugo Orizaba  mousepad Products");
+            cartPrices.push(1500);
+            console.log("you selected Ugo Orizaba mousepad Products");
         break;
         default:
             console.log("invalid option");
@@ -140,22 +207,34 @@ switch (Store) {
     `)
     switch(brand5) {
         case "1":
-            console.log("you selected Gigabyte Aero notebook products");
+            selectedproducts.push("Gigabyte Aero notebook Products");
+            cartPrices.push(1102499);
+            console.log("you selected Gigabyte Aero notebook Products");
         break;
         case "2":
-            console.log("you selected Gigabyte Aorus notebook products");
+            selectedproducts.push("Gigabyte Aorus notebook Products");
+            cartPrices.push(1047359);
+            console.log("you selected Gigabyte Aorus notebook Products");
         break;
         case "3":
-            console.log("you selected Asus notebook products");
+            selectedproducts.push("Asus notebook Products");
+            cartPrices.push(970189);
+            console.log("you selected Asus notebook Products");
         break;
         case "4":
-            console.log("you selected Apple notebook products");
+            selectedproducts.push("Apple notebook Products");
+            cartPrices.push(937109);
+            console.log("you selected Apple notebook Products");
         break;
         case "5":
-            console.log("you selected Lenovo notebook products");
+            selectedproducts.push("Lenovo notebook Products");
+            cartPrices.push(762919);
+            console.log("you selected Lenovo notebook Products");
         break;
         case "6":
-            console.log("you selected Bangho notebook products");
+            selectedproducts.push("Bangho notebook Products");
+            cartPrices.push(363809);
+            console.log("you selected Bangho notebook Products");
         default:
             console.log("invalid option");
     }
@@ -173,22 +252,34 @@ switch (Store) {
 
     switch(brand6) {
         case "1":
-            console.log("you selected Razer desktop products");
+            selectedproducts.push("Razer desktop Products");
+            cartPrices.push(500000);
+            console.log("you selected Razer desktop Products");
         break;
         case "2":
-            console.log("you selected Kairos Pro Ryzen 7 desktop products");
+            selectedproducts.push("Kairos Pro Ryzen 7 desktop Products");
+            cartPrices.push(630109);
+            console.log("you selected Kairos Pro Ryzen 7 desktop Products");
         break;
         case "3":
-            console.log("you selected Kairos Pro Ryzen 3 desktop products");
+            selectedproducts.push("Kairos Pro Ryzen 3 desktop Products");
+            cartPrices.push(566729);
+            console.log("you selected Kairos Pro Ryzen 3 desktop Products");
         break;
         case "4":
-            console.log("you selected Intel i5 desktop products");
+            selectedproducts.push("Intel i5 desktop Products");
+            cartPrices.push(280139);
+            console.log("you selected Intel i5 desktop Products");
         break;
         case "5":
-            console.log("you selected AMD desktop products");
+            selectedproducts.push("AMD desktop Products");
+            cartPrices.push(103079);
+            console.log("you selected AMD desktop Products");
         break;
         case "6":
-            console.log("you selected Nvidia desktop products");
+            selectedproducts.push("Nvidia desktop Products");
+            cartPrices.push(1200000);
+            console.log("you selected Nvidia desktop Products");
         break;
         default:
             console.log("invalid option");
@@ -197,22 +288,41 @@ switch (Store) {
 
 
 
-    }
 
+}
+console.log(selectedproducts);
+console.log(cartPrices);
 
 //ciclos//
 
 let ConfirmBrand = alert("product added successfully");
 
-let payment = "credit card"
+let payment = ("Credit Card")
+
 
 do {
-    payment = prompt("ingrese payment method");
+    payment = prompt("ingrese payment method").toLowerCase();
     console.log(payment);
-}while(payment != "credit card") {
+}while(payment !== "credit card") 
     alert("great choice")
     payment++
-}
+
+//array y metodos//
+
+const addProducts = ["Razer Enki Pro"];
+const youMightLike = addProducts.map(gamingChair => {
+    return gamingChair;
+});
+
+
+alert("You Might Like: " + youMightLike);
+
+let productAdded = prompt(`
+1:Yes
+2:No
+
+`)
+
 
 //funciones//
 
@@ -221,9 +331,10 @@ function impuestos_y_envio(num1, num2) {
     const shipping = 1000;
     const subtotal = num1 + num2;
     const total = subtotal + taxes + shipping;
-    return total;
+    return total; 
 }
 
-const result = impuestos_y_envio(20000, 30000);
-console.log(result + " " +  "total");
-
+const subtotal = cartPrices.reduce((acc, price) => acc + price, 0);
+const total = impuestos_y_envio(subtotal, 0);
+console.log("Subtotal: ARS", subtotal);
+console.log("Total (incluyendo impuestos y envío): ARS ", total);
